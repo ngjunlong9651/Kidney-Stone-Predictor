ed755405c23a33f07587a796c0fd23cef1fd8086
ngjunlong9651 <42053960+ngjunlong9651@users.noreply.github.com>
2023-08-30 18:26:09 +0800

    Testing

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ image = Image.open('placeholder-image.jpg')
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET","POST"])
+st.subheader('Please enter the relevant details below')
 def get_user_input():
     gravity = float(request.form.get("gravity"))
     ph = float(request.form.get("ph"))
@@ -25,12 +25,8 @@ def get_user_input():
     cond = float(request.form.get("cond"))
     urea = float(request.form.get("urea"))
     calc = float(request.form.get("calc"))
-    return(render_template("index.html", result=-0.04191465 + (gravity * -3.94579654e-02) + (ph * -3.94579654e-02) + (osmo * 3.05883681e-04) + (cond * -8.53476004e-02) + (urea * 1.12604054e-03) + (calc * 7.20512124e-01)))
-else:
-    return(render_template("index.html", result="waiting"))
-
-if __name__ == "__main__" :
-app.run()
+    prediction = -0.04191465 + (gravity * -3.94579654e-02) + (ph * -3.94579654e-02) + (osmo * 3.05883681e-04) + (cond * -8.53476004e-02) + (urea * 1.12604054e-03) + (calc * 7.20512124e-01)
+    return prediction
 
 
 
