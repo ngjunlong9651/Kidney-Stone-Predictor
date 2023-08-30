fca943898e49871372e394bfdd016bd7e39b47a8
ngjunlong9651 <42053960+ngjunlong9651@users.noreply.github.com>
2023-08-30 18:43:02 +0800

    Essentially converted Professor Teo's work over to Streamlit, next step is to beautify and optimize the work

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
 
 import pandas as pd
 from sklearn.metrics import accuracy_score
@@ -12,26 +11,25 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import seaborn as sns   
 st.write("This project was prepared fro Prof Teo # Placeholder text ")
-image = Image.open('placeholder-image.jpg')
+image = Image.open('placeholder-image.png')
+st.image(image, caption='Placeholder image',use_column_width=True)
 
-
-app = Flask(__name__)
 
 st.subheader('Please enter the relevant details below')
 def get_user_input():
-    gravity = float(request.form.get("gravity"))
-    ph = float(request.form.get("ph"))
-    osmo = float(request.form.get("osmo"))
-    cond = float(request.form.get("cond"))
-    urea = float(request.form.get("urea"))
-    calc = float(request.form.get("calc"))
+    gravity = st.number_input("gravity")
+    ph = st.number_input("ph")
+    osmo = st.number_input("osmo")
+    cond = st.number_input("cond")
+    urea = st.number_input("urea")
+    calc = st.number_input("calc")
     prediction = -0.04191465 + (gravity * -3.94579654e-02) + (ph * -3.94579654e-02) + (osmo * 3.05883681e-04) + (cond * -8.53476004e-02) + (urea * 1.12604054e-03) + (calc * 7.20512124e-01)
     return prediction
 
+prediction = get_user_input()
 
-
-# In[ ]:
-
+st.subheader('These are the results : ')
+st.write(prediction)
 
 
 
