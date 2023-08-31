b6d91f80db4fe947971150e61758683909e202a3
ngjunlong9651 <42053960+ngjunlong9651@users.noreply.github.com>
2023-08-31 16:35:27 +0800

    changed dependencies

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 import streamlit as st ## Streamlit library for creating the web app
 from PIL import Image ## Image library for displaying images
 from streamlit_option_menu import option_menu ## What should be displayed on the side bar? 
-from streamlit_oauth import OAuth2Component ## For login credentials for potential follow up pages?
+# from streamlit_oauth import OAuth2Component ## For login credentials for potential follow up pages?
 
 ## Creating a side bar for the user
 with st.sidebar:
