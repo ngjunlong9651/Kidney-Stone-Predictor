23024d85501c96c5934feb947da39612d1c4178b
ngjunlong9651 <42053960+ngjunlong9651@users.noreply.github.com>
2023-09-07 12:19:53 +0800

    Bug fix on rounding

--- app.py
+++ app.py
@@ -2,7 +2,9 @@
 # coding: utf-8
 
 ## Importing the libraries required for streamlit
-import os
+## Important note, is that if you want to upload more packages, you need to update the requirements file and re-upload it to Streamlit
+
+import os ## For file management
 import streamlit as st ## Streamlit library for creating the web app
 from PIL import Image ## Image library for displaying images
 from streamlit_option_menu import option_menu ## What should be displayed on the side bar? 
@@ -28,15 +30,16 @@ def get_user_input():
     cond = st.number_input("Please enter the condition here: 👇")
     urea = st.number_input("Please enter the urea here: 👇")
     calc = st.number_input("Please enter the calc here: 👇")
+    ## Prediction here is manually coded as can be seen in the original file provided 
     prediction = -0.04191465 + (gravity * -3.94579654e-02) + (ph * -3.94579654e-02) + (osmo * 3.05883681e-04) + (cond * -8.53476004e-02) + (urea * 1.12604054e-03) + (calc * 7.20512124e-01)
-    return prediction
+    return round(prediction, 5)
 
-prediction = get_user_input()
+prediction = get_user_input() ## Getting user input
 
 st.subheader('Press the button below to submit your information and get results: ')
-if st.button('Submit information!'):
+if st.button('Submit information!'): ## If button pressed, result will be shown. Else nothing will be shown. 
     st.subheader("These are your results: ")
-    st.write(round(prediction),5)
+    st.write(prediction)
 
 
 
